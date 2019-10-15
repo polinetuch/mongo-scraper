88a7e278822606145731d0d8d1a6b15552474f83
Polinet <uchpolinet@gmail.com>
2019-10-15 17:59:52 +1100

    connect handlebars to express app, mongo db and set up port listener

--- server.js
+++ server.js
@@ -8,5 +8,27 @@ var PORT = process.env.PORT || 3000;
 
 var app = express();
 
-var routes = require("./routes")
+// require routes
+var routes = require("./routes");
 
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Connect Handlebars to Express app
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//request to go through route middleware
+app.use(routes);
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Connect to the Mongo database
+mongoose.connect(MONGODB_URI);
+
+// app is listening on the port
+app.listen(PORT, function() {
+  console.log("Listening on http://localhost:" + PORT);
+});
