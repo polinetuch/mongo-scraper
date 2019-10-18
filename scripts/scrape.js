ef4337c7299e25959f4bca0b9e4fd16d3ca81c7d
Polinet <uchpolinet@gmail.com>
2019-10-19 10:01:44 +1100

    used each loop to loop all articles and make them neat using replace and trim

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -7,5 +7,32 @@ var scrape = function() {
         console.log("It's scraping");
 
         var allArticle = [];
-    })
-}
+
+        $(".post-block__title__link").each(function(i, element) {
+            // get the headline of the article
+            var headline = $(this).find("h2").text().trim();
+
+            // get the article url
+            var url = $(this).find("a").attr("href");
+
+            // get the summary of the allArticle
+            var summary = $(this).find().trim();
+
+            if (headline && summary && url) {
+
+                var makeTitleNeat = headline.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+                var makeSummaryNeat = summary.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+
+                var dateToAdd = {
+                    headline: makeTitleNeat,
+                    summary: makeSummaryNeat,
+                    url: "https://techcrunch.com" + url
+                };
+                allArticle.push(dateToAdd);
+            }
+        });
+        return allArticle;
+    });
+};
+
+module.exports = scrape;
