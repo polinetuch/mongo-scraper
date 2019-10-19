b0594f56499fb04a01b73e1c143f5ea7e7847d46
Polinet <uchpolinet@gmail.com>
2019-10-19 14:38:50 +1100

    edited headline.js to create new schema for storing data

---
+++ models/Headline.js
@@ -0,0 +1,34 @@
+// require mongoose to store data
+var mongoose = require("mongoose");
+
+// creating schema
+var Schema = mongoose.Schema;
+
+var headlineSchema = new Schema({
+    headline: {
+        type: String,
+        required: true,
+        unique: { index: { unique: true }}
+    },
+
+    summary: { 
+        type: String,
+        required: true
+    },
+
+    url: { 
+        type: String,
+        unique: true,
+    },
+
+    date: { 
+        type: Date,
+        default: Date.now
+    },
+    saved: { 
+        type: Boolean,
+        default: false
+    }
+});
+
+var Headline = mongoose.model("Headline", headlineSchema)
